ace47fcc0de4c4d21116df47c6c962c4a614900a
dn0rmand <dominique@normand.us>
2024-12-25 08:03:49 -0500

    day 25 done

--- day25.ts
+++ day25.ts
@@ -1,23 +1,56 @@
 import { Day } from './tools/day.ts'
 
-type TInput = string[]
-export class Day25 extends Day<TInput> {
+type TInput = {
+    keys: number[][]
+    locks: number[][]
+}
+
+export class Day25 extends Day<void> {
+    locks: number[][] = []
+    keys: number[][] = []
+
     constructor() {
         super(25)
     }
 
-    loadInput(): TInput {
+    loadInput(): void {
         const data = this.readDataFile()
-        return data
+
+        for (let i = 0; i < data.length; i += 8) {
+            const values = []
+            for (let x = 0; x < 5; x++) {
+                let h = -1
+                for (let y = i; y < i + 7; y++) {
+                    if (data[y][x] === '#') {
+                        h++
+                    }
+                }
+                values.push(h)
+            }
+            if (data[i] === '.....') {
+                // key
+                this.keys.push(values)
+            } else if (data[i] === '#####') {
+                // Lock
+                this.locks.push(values)
+            } else {
+                throw 'Syntax error'
+            }
+        }
     }
 
-    part1(input: TInput): number {
-        return 0
+    fits(lock: number[], key: number[]): number {
+        return lock.some((v, i) => v + key[i] > 5) ? 0 : 1
     }
 
-    part2(input: TInput): string {
+    part1(): number {
+        const total = this.locks.reduce((a, l) => a + this.keys.reduce((a, k) => a + this.fits(l, k), 0), 0)
+        return total
+    }
+
+    part2(): string {
         return 'Merry Christmas'
     }
 }
 
-// new Day25().execute();
+// new Day25().execute()
